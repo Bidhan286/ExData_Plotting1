8d27fa4694403279a84e3f2223fc928b33bf468a
Bidhan286 <bidhan286@gmail.com>
2023-05-23 10:47:31 +0530

    plot1

--- Project1/plot1.R
+++ Project1/plot1.R
@@ -3,7 +3,7 @@ library("data.table")
 setwd("C:/Users/bidhan.chakraborty/OneDrive - Kantar/Documents/Project1/Project1")
 #Reads in data from file then subsets data for specified dates
 df_power <- fread("household_power_consumption.txt", na.strings="?")
-df_power <- df_power[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+df_power <- df_power[(Date >= "1/2/2007") & (Date <= "2/2/2007"),]
 png("plot1.png", width=480, height=480)
 ## Plot 1
 hist(df_power[, Global_active_power], main="Global Active Power", 
